815108daed7384f97f8de8cc9234bad6dad1fafe
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-04-12 09:08:06 +0300

    Create a basic shell

--- main.c
+++ main.c
@@ -8,5 +8,31 @@
 
 int main(void)
 {
+	char prompt[MAX_INPUT_LENGTH];
+	pid_t pid;
+
+	while (1)
+	{
+		printf("$ ");
+		if (fgets(prompt, MAX_INPUT_LENGTH, stdin) == NULL)
+			break;
+		prompt[strcspn(prompt, "\n")] = '\0';
+		pid = fork();
+		if (pid == 0)
+		{
+			execlp(prompt, prompt, NULL);
+			fprintf(stderr, "Command not found.\n");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+		{
+			fprintf(stderr, "Failed to fork.\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
 	return (0);
 }
--- main.h
+++ main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_LENGTH 256
 
 #endif /* MAIN_H */
