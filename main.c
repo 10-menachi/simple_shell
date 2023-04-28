c4bd3ea6e22e971d1d58cbeeaa3631efe9fa2cd8
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-04-28 10:45:58 +0300

    Fix bug in code

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include "shell.h"
+
 /**
  * main - entry point
+ *
  * Return: 0
  */
+
 int main(void)
 {
 	char prompt[MAX_INPUT_LENGTH], *args[MAX_INPUT_LENGTH / 2 + 1];
@@ -10,7 +13,9 @@ int main(void)
 	char error_msg_fork[] = "Failed to fork.\n";
 	pid_t pid;
 	int show_prompt = isatty(STDIN_FILENO);
+	int pipefd[2];
 
+	pipe(pipefd);
 	while (1)
 	{
 		if (show_prompt)
@@ -24,6 +29,9 @@ int main(void)
 		pid = fork();
 		if (pid == 0)
 		{
+			close(pipefd[0]);
+			dup2(pipefd[1], STDOUT_FILENO);
+			close(pipefd[1]);
 			execvp(args[0], args);
 			write(STDERR_FILENO, error_msg, sizeof(error_msg));
 			exit(EXIT_FAILURE);
@@ -35,6 +43,9 @@ int main(void)
 		}
 		else
 		{
+			close(pipefd[1]);
+			dup2(pipefd[0], STDIN_FILENO);
+			close(pipefd[0]);
 			if (_strcmp(args[0], "env") == 0)
 				env_builtin();
 			wait(NULL);
