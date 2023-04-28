d3f99669ba4369f7416a6708d24e8a4b6e18fb82
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-04-28 11:30:08 +0300

    Fix linter errors

--- shell.h
+++ shell.h
@@ -18,6 +18,6 @@ ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
 int _strcmp(char *s1, char *s2);
 char *strtrim(char *str);
 char *find_path(char *command);
-void handle_error(char* command_name, int status);
+void handle_error(char *command_name, int status);
 void env_builtin(void);
 #endif /* SHELL_H */
