8cf41fae87e5f40f7407115c5d2df7c4c6179cec
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-04-28 08:15:46 +0300

    Implement simple shell

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * main - Entry Point
+ * main - entry point
  *
  * Return: 0
  */
@@ -9,7 +9,7 @@
 int main(void)
 {
 	char prompt[MAX_INPUT_LENGTH];
-	char *args[MAX_INPUT_LENGTH / 2 + 1];
+	char *args[2];
 	char error_msg[] = "Command not found.\n";
 	char error_msg_fork[] = "Failed to fork.\n";
 	pid_t pid;
@@ -20,7 +20,8 @@ int main(void)
 		if (fgets(prompt, MAX_INPUT_LENGTH, stdin) == NULL)
 			break;
 		prompt[strcspn(prompt, "\n")] = '\0';
-		parse_input(prompt, args);
+		args[0] = prompt;
+		args[1] = NULL;
 		pid = fork();
 		if (pid == 0)
 		{
@@ -35,10 +36,7 @@ int main(void)
 		}
 		else
 		{
-			if (_strcmp(args[0], "env") == 0)
-				env_builtin();
-			else
-				wait(NULL);
+			wait(NULL);
 		}
 	}
 	return (0);
